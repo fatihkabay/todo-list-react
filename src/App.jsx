f0ee9e25a753a8438ac60b63e8ec3e13d36d97fa
Fatih Kabay <mfthkabay@gmail.com>
2022-07-23 23:51:09 +0300

    create localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import "./App.css";
@@ -6,6 +6,22 @@ const App = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
   const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    const todosLocalStorage = localStorage.getItem('todos');
+    if (todosLocalStorage != null) {
+      const cacheTodos = JSON.parse(todosLocalStorage);
+      setTodos(cacheTodos);
+    } 
+
+  }, []);
+
+  const onChangeTodos = (todos) => {
+    const todosStringify = JSON.stringify(todos);
+    localStorage.setItem('todos', todosStringify);
+    setTodos(todos);
+  }
+
   return (
      <div id="root">
       <div className="container">
@@ -15,18 +31,16 @@ const App = () => {
           input={input}
           setInput={setInput}
           todos={todos}
-          setTodos={setTodos}
+          setTodos={onChangeTodos}
           editTodo={editTodo}
           setEditTodo={setEditTodo}
         />
-        <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+        <TodoList todos={todos} setTodos={onChangeTodos} setEditTodo={setEditTodo} />
         <div className="footer-button">
           <div className="list-info">You Have {todos.filter((p) => !p.completed).length} Pending Tasks</div>
           <button
             disabled={todos.length < 1}
-            onClick={() => {
-              setTodos([]);
-            }}
+            onClick={() => onChangeTodos([])}
             className="all-delete-button"
           >
              Clear All
